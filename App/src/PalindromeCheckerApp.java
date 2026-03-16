155b02ca25908b7a35396c179e51a945df716153
csclcmv <ak8366@srmist.edu.in>
2026-03-16 15:27:45 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,29 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App Management System");
-        System.out.println("Modify this logic to usecase 2");
+
+        // UC1 - Application Entry
+        System.out.println("=================================");
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Application Version: 1.0");
+        System.out.println("=================================");
+
+        // UC2 - Hardcoded Palindrome Check
+        String word = "madam";
+
+        String reversed = "";
+
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        System.out.println("Word to check: " + word);
+
+        if(word.equals(reversed)) {
+            System.out.println("Result: It is a Palindrome");
+        } else {
+            System.out.println("Result: It is NOT a Palindrome");
+        }
+
     }
 }
