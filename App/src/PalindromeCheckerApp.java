c82c4670838dfc9cefc863e128e845650766258f
csclcmv <ask03082007@gmail.com>
2026-03-16 15:08:13 +0530

    [Name] Added UC1 Application Entry

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,13 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App Management System");
-        System.out.println("Modify this logic to usecase 2");
+
+        System.out.println("=================================");
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Application Version : 1.0");
+        System.out.println("=================================");
+
+        System.out.println("Application Started Successfully!");
+
     }
 }
